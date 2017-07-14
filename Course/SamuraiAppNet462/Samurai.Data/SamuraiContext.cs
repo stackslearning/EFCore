a41e6ba6d5787e208d03c0021a4c962dc4471c02
Smooth Learning <smoothlearning@users.noreply.github.com>
2017-07-13 21:39:29 -0700

    Add EF Core 1.1.0 packages

--- Course/SamuraiAppNet462/Samurai.Data/SamuraiContext.cs
+++ Course/SamuraiAppNet462/Samurai.Data/SamuraiContext.cs
@@ -1,15 +1,11 @@
-﻿using Samurai.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Samurai.Domain;
 
 namespace Samurai.Data
 {
 	public class SamuraiContext : DbContext
 	{
-		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Domain.Samurai> Samurais { get; set; }
 		public DbSet<Battle> Battles { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
 	}
