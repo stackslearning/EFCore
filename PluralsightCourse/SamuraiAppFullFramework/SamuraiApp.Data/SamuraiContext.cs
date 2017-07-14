4a356f4286471ee753a0303eae4a5266e50d6818
Smooth Learning <smoothlearning@users.noreply.github.com>
2017-07-13 20:11:50 -0700

    Reference Domain from Data prj and include Samuraicontext

---
+++ PluralsightCourse/SamuraiAppFullFramework/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,16 @@
+﻿using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.Data
+{
+	public class SamuraiContext : DbContext
+	{
+		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Battle> Battles { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
+	}
+}
