b8eee72b8e707974c0a26dcde3b580516e5023c7
Smooth Learning <smoothlearning@users.noreply.github.com>
2017-07-15 17:40:38 -0700

    Add package Microsoft.EntityFrameworkCore.Tools 1.1.0 and first DB migration
    * Why did it auto change the other EF Core pkges to 1.1.1? Good question!
    - Added Sql Server db provider with local db connection string

---
+++ Course/SamuraiAppNet462/Samurai.Data/Migrations/20170716001942_Init.Designer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Samurai.Data;
+
+namespace Samurai.Data.Migrations
+{
+    [DbContext(typeof(SamuraiContext))]
+    [Migration("20170716001942_Init")]
+    partial class Init
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Samurai.Domain.Battle", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("EndDate");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Battles");
+                });
+
+            modelBuilder.Entity("Samurai.Domain.Quote", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("SamuraiId");
+
+                    b.Property<string>("Text");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("SamuraiId");
+
+                    b.ToTable("Quotes");
+                });
+
+            modelBuilder.Entity("Samurai.Domain.Samurai", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BattleId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("BattleId");
+
+                    b.ToTable("Samurais");
+                });
+
+            modelBuilder.Entity("Samurai.Domain.Quote", b =>
+                {
+                    b.HasOne("Samurai.Domain.Samurai", "Samurai")
+                        .WithMany("Quotes")
+                        .HasForeignKey("SamuraiId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Samurai.Domain.Samurai", b =>
+                {
+                    b.HasOne("Samurai.Domain.Battle")
+                        .WithMany("Samurais")
+                        .HasForeignKey("BattleId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
--- Course/SamuraiAppNet462/Samurai.Data/SamuraiContext.cs
+++ Course/SamuraiAppNet462/Samurai.Data/SamuraiContext.cs
@@ -8,5 +8,11 @@ namespace Samurai.Data
 		public DbSet<Domain.Samurai> Samurais { get; set; }
 		public DbSet<Battle> Battles { get; set; }
 		public DbSet<Quote> Quotes { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var cnn = "Server=(localdb)\\mssqllocaldb;Database=SamuraiData;Trusted_Connection=True";
+			optionsBuilder.UseSqlServer(cnn);
+		}
 	}
 }
