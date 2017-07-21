56376b15995089b068b705bd67aed6b559385a08
Smooth Learning <smoothlearning@users.noreply.github.com>
2017-07-20 21:07:14 -0700

    Add Samurai.Console project

---
+++ Course/SamuraiAppNet462/Samurai.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using Samurai.Data;
+
+using SamuraiDomain = Samurai.Domain.Samurai;
+
+namespace Samurai.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			InsertSamurai();
+		}
+
+		private static void InsertSamurai()
+		{
+			var sam1 = new SamuraiDomain { Name = "Sam1" };
+			using (var db = new SamuraiContext())
+			{
+				db.Samurais.Add(sam1);
+				db.SaveChanges();
+			}
+		}
+	}
+}
