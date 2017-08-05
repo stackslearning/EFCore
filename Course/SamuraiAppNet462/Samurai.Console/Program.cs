e50afc778f9813fd3c1c3c7b5e97ce6ef25d7f4d
Smooth Learning <smoothlearning@users.noreply.github.com>
2017-08-04 21:47:23 -0700

    Reorganize insert operations

--- Course/SamuraiAppNet462/Samurai.Console/Program.cs
+++ Course/SamuraiAppNet462/Samurai.Console/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Logging;
 using Samurai.Data;
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using SamuraiDomain = Samurai.Domain.Samurai;
 
 namespace Samurai.Console
@@ -10,19 +12,48 @@ namespace Samurai.Console
 	{
 		static void Main(string[] args)
 		{
-			InsertSamurai();
+			Insert();
+			Select();
 		}
 
-		private static void InsertSamurai()
+		private static void Insert()
 		{
-			var sam1 = new SamuraiDomain { Name = "Sam1" };
-			using (var db = new SamuraiContext())
+			InsertSamurai();
+			void InsertSamurai()
 			{
-				db.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+				var sam1 = new SamuraiDomain { Name = "Sam " + Guid.NewGuid() };
+				using (var context = new SamuraiContext())
+				{
+					context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+					context.Samurais.Add(sam1);
+					context.SaveChanges();
+				}
+			}
 
-				db.Samurais.Add(sam1);
-				db.SaveChanges();
+			InsertSamuraisInOneTripToDatabase();
+			void InsertSamuraisInOneTripToDatabase()
+			{
+				var samurais = new List<SamuraiDomain>
+				{
+					new SamuraiDomain { Name = "Bulk Sam "  + Guid.NewGuid() },
+					new SamuraiDomain { Name = "Bulk Sam 2"  + Guid.NewGuid() }
+				};
+				using (var context = new SamuraiContext())
+				{
+					context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+					context.Samurais.AddRange(samurais);
+					context.SaveChanges();
+				}
 			}
 		}
+
+		private static void Select()
+		{
+			using (var context = new SamuraiContext())
+			{
+				var samurais = context.Samurais.ToList();
+			}
+		}
+
 	}
 }
